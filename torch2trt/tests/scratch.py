17af86d2367d01500b80130c25560bc949982bb4
Michael <michael3795@gmail.com>
2020-01-07 16:51:10 -0600

    st

--- torch2trt/tests/scratch.py
+++ torch2trt/tests/scratch.py
@@ -1,18 +1,27 @@
 import torch
-import torch2trt
-from transformers import GPT2LMHeadModel, GPT2Config
+# import torch2trt
+import tensorrt as trt
+import importlib
+
+importlib.reload(torch2trt)
+from .gpt2 import GPT2LMHeadModel, GPT2Config
 
 config = GPT2Config(n_layer=1, n_head=1, n_embd=2, vocab_size=10)
 model = GPT2LMHeadModel(config)
 input_names = ["input_ids", "past"]
-input_shapes = [(1, -1), (2, 1, config.n_head, -1, config.n_embd)]
+input_shapes = [(1, -1), (config.n_layer, 2, 1, config.n_head, -1, config.n_embd)]
 inputs = []
+inputs.append(torch.zeros((1, 1), dtype=torch.int32))
+inputs.append(torch.zeros(tuple(x if x != -1 else 1 for x in input_shapes[1])))
 opt_profile = {}
-for name, s in zip(input_names, input_shapes):
-    inputs.append(torch.zeros(tuple(x if x != -1 else 1 for x in s)))
-    opt_profile[name] = [(x, x, x) for x in s]
-opt_profile["input_ids"][1] = (1, 1, 1024)
-opt_profile["past"][3] = (1, 256, 1024)
+opt_profile["input_ids"] = [(x, x, x) for x in inputs[0].shape]
+opt_profile["past"] = [(x, x, x) for x in inputs[1].shape]
+opt_profile["input_ids"][-1] = (1, 1, 1024)
+opt_profile["past"][-2] = (0, 256, 1024)
 
-torch2trt.torch2trt(model, inputs=inputs, input_names=input_names, input_shapes=input_shapes, flags=0, fp16_mode=True,
-                    optimization_profile=opt_profile)
+# print(model(input_ids=inputs[0].to(torch.long),past=inputs[1]))
+# print(inputs[0].shape,inputs[1].shape)
+flags = 1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+model_trt = torch2trt.torch2trt(model, inputs=inputs, input_names=input_names, input_shapes=input_shapes, flags=flags,
+                                fp16_mode=True,
+                                optimization_profile=opt_profile)
