stupid broadcast type bug

tests/gpt2.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 from torch.nn import ModuleList
 from transformers.configuration_gpt2 import GPT2Config
 from transformers.modeling_gpt2 import load_tf_weights_in_gpt2
-from transformers.modeling_utils import PreTrainedModel, Conv1D, prune_conv1d_layer
+from transformers.modeling_utils import PreTrainedModel, prune_conv1d_layer
 
 logger = logging.getLogger(__name__)
 
@@ -24,6 +24,27 @@ GPT2_PRETRAINED_MODEL_ARCHIVE_MAP = {
 # TODO use BERT plugins .cu once i get this working..
 def gelu(x):
     return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+
+
+class Conv1D(nn.Module):
+    def __init__(self, nf, nx):
+        """ Conv1D layer as defined by Radford et al. for OpenAI GPT (and also used in GPT-2)
+            Basically works like a Linear layer but the weights are transposed
+        """
+        super(Conv1D, self).__init__()
+        self.nf = nf
+        w = torch.empty(nx, nf)
+        nn.init.normal_(w, std=0.02)
+        self.weight = nn.Parameter(w)
+        self.bias = nn.Parameter(torch.zeros(nf))
+
+    def forward(self, x):
+        nx = self.weight.shape[0]
+        assert x.size(-1) == nx
+        size_out = x.size()[:-1] + (self.nf,)
+        x = torch.addmm(self.bias, x.view(-1, nx), self.weight)
+        x = x.view(size_out[0], -1, self.nf)
+        return x
 
 
 # torch.nn.Conv1d

torch2trt/trt_module.py
@@ -33,6 +33,7 @@ class TRTModule(torch.nn.Module):
         # these must be in order
         self.input_names = input_names
         self.output_names = output_names
+        self.bindings = [None] * self.engine.num_bindings
 
     def _on_state_dict(self, state_dict, prefix, local_metadata):
         state_dict[prefix + 'engine'] = bytearray(self.engine.serialize())
@@ -51,23 +52,39 @@ class TRTModule(torch.nn.Module):
         self.output_names = state_dict[prefix + 'output_names']
 
     def forward(self, *inputs):
-        bindings = self.get_bindings({name: value for name, value in
-                                      zip(self.input_names, inputs)})
+        self.set_input_bindings(inputs)
 
         self.context.execute_v2([t.data_ptr()
-                                 if t is not None else 0 for t in bindings])
+                                 if t is not None else 0 for t in self.bindings])
 
-        outputs = [bindings[self.engine.get_binding_index(oname)] for oname in self.output_names]
+        outputs = [self.bindings[self.engine.get_binding_index(oname)] for oname in self.output_names]
         return outputs[0] if len(outputs) == 1 else tuple(outputs)
 
-    def get_bindings(self, input_dict) -> List[Optional[torch.Tensor]]:
-        bindings = [None] * self.engine.num_bindings
+    # Can't call this before reset_bindings
+    def set_input_bindings(self, input_dict):
+        if isinstance(input_dict, (list, tuple)):
+            input_dict = {k: v for k, v in zip(self.input_names, input_dict)}
+        # Step 2: execution bindings (including shape bindings for dynamic inputs)
+        for idx in range(self.engine.num_bindings):
+            if self.engine.is_execution_binding(idx):
+                # print(f"Setting execution binding {name}, {idx}")
+                if self.engine.binding_is_input(idx):
+                    name = self.engine.get_binding_name(idx)
+                    self.bindings[idx] = input_dict[name]
+
+        # assert self.context.all_binding_shapes_specified
+
+    # After calling this once, only set_input_bindings is needed on forward passes
+    def reset_bindings(self, input_dict):  # -> List[Optional[torch.Tensor]]:
+        if isinstance(input_dict, (list, tuple)):
+            input_dict = {k: v for k, v in zip(self.input_names, input_dict)}
+        self.bindings = [None] * self.engine.num_bindings
         # Step 1: shape inputs
         for idx in range(self.engine.num_bindings):
             if self.engine.binding_is_input(idx) and \
                     self.engine.is_shape_binding(idx):
                 name = self.engine.get_binding_name(idx)
-                print(f"Setting shape input {name}, {idx} to {input_dict[name]}")
+                print(f"Setting {name}:{idx} shape input to {input_dict[name]}")
                 self.context.set_shape_input(idx, input_dict[name])
 
         assert self.context.all_shape_inputs_specified
@@ -77,11 +94,11 @@ class TRTModule(torch.nn.Module):
             if self.engine.binding_is_input(idx):
                 name = self.engine.get_binding_name(idx)
                 if -1 in self.engine.get_binding_shape(idx):  # ?? not sure if this is right
-                    print(f"Setting binding shape {name}, {idx} to {input_dict[name].shape}")
+                    print(f"Setting {name}:{idx} binding shape to {input_dict[name].shape}")
                     self.context.set_binding_shape(idx, input_dict[name].shape)
                 if self.engine.is_execution_binding(idx):
-                    print(f"Setting execution binding {name}, {idx}")
-                    bindings[idx] = input_dict[name]
+                    print(f"Setting {name}:{idx} execution binding (input)")
+                    self.bindings[idx] = input_dict[name]
 
         assert self.context.all_binding_shapes_specified
 
@@ -93,10 +110,8 @@ class TRTModule(torch.nn.Module):
                 dtype = torch_dtype_from_trt(self.engine.get_binding_dtype(idx))
                 device = torch_device_from_trt(self.engine.get_location(idx))
                 shape = tuple(self.context.get_binding_shape(idx))  # Infer output shape using TRT
-                print(f"Computed output shape {shape} for {name}")
-                bindings[idx] = torch.empty(size=shape, dtype=dtype, device=device)
-
-        return bindings
+                print(f"Computed {name}:{idx} output shape {shape}")
+                self.bindings[idx] = torch.empty(size=shape, dtype=dtype, device=device)
 
     def enable_profiling(self):
         if not self.context.profiler: